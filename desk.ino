15e2ba455212ab7525cd236bdd414ad9c8f5f6e5
Eric Schultz <wwahammy@gmail.com>
2014-02-18 21:17:49 -0600

    Added actuator pin setup, saving settings and more

--- desk.ino
+++ desk.ino
@@ -1,6 +1,7 @@
 #include <LiquidCrystal.h>
 #include <EEPROM.h>
 #include <Keypad.h>
+#include "Actuator.h"
 
 
 /** begin non-pin consts **/
@@ -26,32 +27,32 @@ const int KEYPAD_COLS = 3;
 
 
 //actuator
-const int EXTEND_ACTUATOR_PIN =
-const int RETRACT_ACTUATOR_PIN =
-const int ACTUATOR_POSITION_PIN =
+const int EXTEND_ACTUATOR_PIN = 0;
+const int RETRACT_ACTUATOR_PIN =  0;
+const int POSITION_ACTUATOR_PIN =  0;
 
 
 //lcd
-const int LCD_REGISTER_SELECT_PIN = ;
-const int LCD_RW_PIN = ;
-const int LCD_ENABLE_PIN = ;
+const int LCD_REGISTER_SELECT_PIN = 0;
+const int LCD_RW_PIN = 0;
+const int LCD_ENABLE_PIN = 0;
 
 
 // keypad
-const int UP_BUTTON_PIN=
-const int DOWN_BUTTON_PIN=
+const int UP_BUTTON_PIN= 0;
+const int DOWN_BUTTON_PIN= 0;
 
-
-#define ROW0_PIN 0
-#define ROW1_PIN 0
-#define COL0_PIN 0
-#define COL1_PIN 0
-#define COL2_PIN 0
+const int ROW0_PIN=0;
+const int ROW1_PIN=0;
+const int COL0_PIN=0;
+const int COL1_PIN= 0;
+const int COL2_PIN =0;
 
 
 /** end pins **/
 
-
+const int ROWS=2;
+const int COLS=3;
 
 
 char keys[ROWS][COLS] = {
@@ -59,9 +60,14 @@ char keys[ROWS][COLS] = {
 	{'D', '3', '4'}
 };
 
-byte rowPins[ROWS] = {ROW0_PIN, ROW1_PIN};
-byte columnPins[COLS] = {COL0_PIN, COL1_PIN, COL2_PIN};
-Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS ); 
+byte row_pins[ROWS] = {ROW0_PIN, ROW1_PIN};
+byte col_pins[COLS] = {COL0_PIN, COL1_PIN, COL2_PIN};
+Keypad keypad = Keypad( makeKeymap(keys), row_pins, col_pins, ROWS, COLS ); 
+
+Actuator actuator = Actuator(EXTEND_ACTUATOR_PIN, RETRACT_ACTUATOR_PIN, POSITION_ACTUATOR_PIN);
+
+int minimum_height_in_mm = 0;
+int maximium_height_in_mm = 0;
  
  
 int saved_setting_values[] = { 0, 341, 682, 1023};
@@ -70,26 +76,42 @@ void setup()
 {
 	Serial.begin(9600);
 	load_all_settings();
+	keypad.addTransitionListener(&handle_transition);
 	
 }
 
 void loop()
 {
+	//just run us
+	keypad.getKeys();
+	//if we need to handle a keyPress
 }
 
-void load_setting(int saved_setting_number)
+void load_setting(byte saved_setting_number)
 {
 	int value, ever_saved;
 	if (saved_setting_number >= NUM_OF_SETTINGS)
 		return;
 		
 	
-	value = read_eeprom(saved_setting_number + SAVED_SETTING_START_LOCATION);
-	ever_saved = read_eeprom(saved_setting_number + SAVED_SETTING_START_LOCATION);
+	value = read_eeprom(savedSettingLocation(saved_setting_number));
+	ever_saved = read_eeprom(hasEverBeenSavedLocation(saved_setting_number));
 	if (ever_saved == 255)
 		return;
 	
-	saved_setting_value[saved_setting_number] = value;
+	saved_setting_values[saved_setting_number] = value;
+}
+
+
+
+void save_setting(byte saved_setting_number)
+{
+  if (saved_setting_number >= NUM_OF_SETTINGS){
+	return;
+  }
+  
+  write_eeprom(savedSettingLocation(saved_setting_number), actuator.getCurrentPosition());
+  write_eeprom(hasEverBeenSavedLocation(saved_setting_number), 1);
 }
 
 void load_all_settings()
@@ -104,9 +126,37 @@ void load_all_settings()
 int read_eeprom(int location)
 {
 	int value;
-	value = EEPROM.read(location)
+	value = EEPROM.read(location);
 	Serial.print("Read EEPROM location ");
 	Serial.print(location, DEC);
 	Serial.print(" value was ");
 	Serial.println(value, DEC);
 }
+
+void write_eeprom(int location, int value)
+{
+	EEPROM.write(location, value);
+	Serial.print("Wrote EEPROM location ");
+	Serial.print(location, DEC);
+	Serial.print(" with value ");
+	Serial.println(value, DEC);
+}
+	
+
+int savedSettingLocation(byte savedSettingNumber)
+{
+	return (savedSettingNumber + SAVED_SETTING_START_LOCATION) * sizeof(int);
+}
+
+int hasEverBeenSavedLocation(byte savedSettingNumber)
+{
+	return (savedSettingNumber + HAS_SETTING_EVER_BEEN_SAVED_LOCATION) *sizeof(int);
+}
+
+void handle_transition(char key, KeyState from, KeyState to)
+{
+}
+
+
+
+
