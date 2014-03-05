f2a6567adc2e15782e74826172bbc2b306cdc3bb
Eric Schultz <wwahammy@gmail.com>
2014-03-04 21:15:04 -0600

    LcdAdapter is fixed and compiles

--- LcdAdapter.cpp
+++ LcdAdapter.cpp
@@ -2,41 +2,47 @@
 
 LcdAdapter::LcdAdapter(uint8_t rs, uint8_t rw, uint8_t enable,
 			uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3, int marquee){
-	innerLcd = LiquidCrystal(rs, rw, enable, d0, d1, d2, d3);
+	innerLcd = new LiquidCrystal(rs, rw, enable, d0, d1, d2, d3);
 	marqueeTime = marquee;
 }
 
 LcdAdapter::LcdAdapter(uint8_t rs, uint8_t enable,
 			uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3, int marquee){
-	innerLcd = LiquidCrystal(rs, enable, d0, d1, d2, d3);
+	innerLcd = new LiquidCrystal(rs, enable, d0, d1, d2, d3);
 	marqueeTime = marquee;
+}
+
+LcdAdapter::~LcdAdapter()
+{
+	delete innerLcd;
+	innerLcd = NULL;
 }
 
 void LcdAdapter::handleEvents(){
 	if (inMarquee && ((beganMarqueeTime + marqueeTime) < millis())){
-		innerLcd.clear();
-		innerLcd.print(printText);
+		innerLcd->clear();
+		innerLcd->print(printText);
 		inMarquee = false;
 	}
 }
 
-size_t print(String text){
+size_t LcdAdapter::print(String text){
 	printText = text;
 	if (!inMarquee){
-		innerLcd.clear();
-		innerLcd.print(printText);
+		innerLcd->clear();
+		innerLcd->print(printText);
 	}
 	
 }
 
-size_t marquee(String text){
+size_t LcdAdapter::marquee(String text){
 	marqueeText = text;
 	inMarquee = true;
 	beganMarqueeTime = millis();
-	innerLcd.clear();
-	innerLcd.print(marqueeText);
+	innerLcd->clear();
+	innerLcd->print(marqueeText);
 }
 
-void begin(uint8_t cols, uint8_t rows){
-	innerLcd.begin(cols, rows);
+void LcdAdapter::begin(uint8_t cols, uint8_t rows){
+	innerLcd->begin(cols, rows);
 }
--- LcdAdapter.h
+++ LcdAdapter.h
@@ -1,33 +1,36 @@
-#include <LiquidCrystal.h>
+
 
 #ifndef LCDADAPTER_H
 #define LCDADAPTER_H
+#include "LiquidCrystal.h"
 
 #include "Arduino.h"
-#include <LiquidCrystal.h>
-
+#include "stdio.h"
 
 
 class LcdAdapter
 {
 	public:
+
 		LcdAdapter(uint8_t rs, uint8_t rw, uint8_t enable,
 			uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3, int marqueeLength);
 		LcdAdapter(uint8_t rs, uint8_t enable,
 			uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3, int marqueeLength);
 		void begin(uint8_t cols, uint8_t rows);
 		void handleEvents();
-		size_t print(String);
-		size_t marquee(String);
+		size_t print(String text);
+		size_t marquee(String text);
+		~LcdAdapter();
 	private:
-		LiquidCrystal innerLcd;
-		int marqueeTime;
+		LiquidCrystal * innerLcd;
+        int marqueeTime;
 		String printText;
 		String marqueeText;
 		unsigned long beganMarqueeTime;
 		bool inMarquee;
+
 };
 
 
-#endif LCDADAPTER_H
+#endif
 
