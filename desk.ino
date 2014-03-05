d6da836c30e035174adbcb5e8a0c72a684177845
Eric Schultz <wwahammy@gmail.com>
2014-03-04 21:17:23 -0600

    Pins setup, actuator values wrapped, saving, loading and the LCDing happening

--- desk.ino
+++ desk.ino
@@ -1,10 +1,15 @@
-
+#include <LiquidCrystal.h>
 #include <EEPROM.h>
 #include <Keypad.h>
 #include "Actuator.h"
 #include "LcdAdapter.h"
 
 
+struct TransitionOccurred {
+	char key;
+	KeyState fromState;
+	KeyState toState;
+};
 /** begin non-pin consts **/
 //settings
 const int NUM_OF_SETTINGS = 4;
@@ -31,7 +36,8 @@ const int KEYPAD_COLS = 3;
 //actuator
 const int EXTEND_ACTUATOR_PIN = 0;
 const int RETRACT_ACTUATOR_PIN =  1;
-const int POSITION_ACTUATOR_PIN =  //A0;
+//analog
+const int POSITION_ACTUATOR_PIN =  0;
 
 
 //lcd
@@ -69,38 +75,41 @@ byte col_pins[COLS] = {COL0_PIN, COL1_PIN, COL2_PIN};
 Keypad keypad = Keypad( makeKeymap(keys), row_pins, col_pins, ROWS, COLS ); 
 
 Actuator actuator = Actuator(EXTEND_ACTUATOR_PIN, RETRACT_ACTUATOR_PIN, POSITION_ACTUATOR_PIN);
+LcdAdapter lcd = LcdAdapter(LCD_REGISTER_SELECT_PIN, LCD_RW_PIN, LCD_ENABLE_PIN, LCD_D4, LCD_D5, LCD_D6, LCD_D7, MARQUEE_TIME);
 
 int minimum_height_in_mm = 0;
 int maximium_height_in_mm = 0;
  
 bool didTransitionOccur = false;
-TransitionOccured currentTransition;
+TransitionOccurred currentTransition;
  
  
-int saved_setting_values[] = { 0, 341, 682, 1023};
+int saved_setting_values_defaults[] = { 0, 341, 682, 1023};
 
-typedef struct {
-	char key;
-	KeyState fromState;
-	KeyState toState;
-} TransitionOccurred;
+
 
 
 void setup()
 {
 	Serial.begin(9600);
+	setupPins();
 	load_all_settings();
 	keypad.addTransitionListener(&handle_transition);
-	
+	lcd.begin(NUM_OF_LCD_CHARS, NUM_OF_LCD_LINES);
 }
+
+
 
 void loop()
 {
 	//just run us
 	//need to run 
 	keypad.getKeys();
+	lcd.handleEvents();
 	
+	lcd.print( getPrettyActuatorValue());
 	//if we need to handle a keyPress, we do it here
+	
 	if (didTransitionOccur)
 	{
 		switch (currentTransition.key)
@@ -112,15 +121,21 @@ void loop()
 					actuator.goDown();
 					break;
 			default:
-					//we have something a settings press or hold
+					//we we've moved from PRESSED to HOLD or RELEASE
 					if(currentTransition.fromState == PRESSED && currentTransition.toState == HOLD)
 					{
 						//we save!
-					
+					    save_setting(getByteFromDigit(currentTransition.key));
+						lcd.marquee("Saving " + getPrettyActuatorValue() + " to save space " + currentTransition.key);
 					}
 					else if(currentTransition.fromState == PRESSED && currentTransition.toState == RELEASED)
 					{
 						//we go to!
+						int value = load_setting(getByteFromDigit(currentTransition.key));
+						if (value >= 0)
+						{
+							actuator.goTo(value);
+						}
 					}
 		}//keypress handling
 	}
@@ -128,19 +143,19 @@ void loop()
 	clearTransitionState();
 }
 
-void load_setting(byte saved_setting_number)
+int load_setting(byte saved_setting_number)
 {
 	int value, ever_saved;
 	if (saved_setting_number >= NUM_OF_SETTINGS)
-		return;
+		return -1;
 		
 	
 	value = read_eeprom(savedSettingLocation(saved_setting_number));
 	ever_saved = read_eeprom(hasEverBeenSavedLocation(saved_setting_number));
 	if (ever_saved == 255)
-		return;
+		return saved_setting_values_defaults[saved_setting_number];
 	
-	saved_setting_values[saved_setting_number] = value;
+	return value;
 }
 
 
@@ -183,6 +198,11 @@ void write_eeprom(int location, int value)
 	Serial.println(value, DEC);
 }
 	
+byte getByteFromDigit(char digit)
+{
+	return digit - '0';
+}
+
 
 int savedSettingLocation(byte savedSettingNumber)
 {
@@ -195,6 +215,11 @@ int hasEverBeenSavedLocation(byte savedSettingNumber)
 }
 
 
+//someday will get you the value in inches or millimeters. Currently just gives you the number
+String getPrettyActuatorValue()
+{
+	return String(actuator.getCurrentPosition());
+}
 
 void handle_transition(char key, KeyState from, KeyState to)
 {
@@ -207,6 +232,25 @@ void handle_transition(char key, KeyState from, KeyState to)
 void clearTransitionState()
 {
 	didTransitionOccur = false;
+}
+
+void setupPins()
+{
+	pinMode(EXTEND_ACTUATOR_PIN, OUTPUT);
+	pinMode(RETRACT_ACTUATOR_PIN,OUTPUT);
+	pinMode(LCD_REGISTER_SELECT_PIN,OUTPUT);
+	pinMode(LCD_RW_PIN,OUTPUT);
+	pinMode(LCD_ENABLE_PIN,OUTPUT);
+	pinMode(LCD_D4,OUTPUT);
+	pinMode(LCD_D5,OUTPUT);
+	pinMode(LCD_D6,OUTPUT);
+	pinMode(LCD_D7,OUTPUT);
+	
+	pinMode(ROW0_PIN,INPUT);
+	pinMode(ROW1_PIN, INPUT);
+	pinMode(COL0_PIN, INPUT);
+	pinMode(COL1_PIN, INPUT);
+	pinMode(COL2_PIN, INPUT);
 }
 
 
